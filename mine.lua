14647071adbf007b2f653dc1d5cf22380b22695f
Timothy Roe, Jr <timroejr@msn.com>
2017-06-13 21:29:04 -0700

    Initial Commit

---
+++ mine.lua
@@ -0,0 +1,131 @@
+function text()
+        shell.run("clear")
+        print("Fuel Level: ".. turtle.getFuelLevel())
+        print ""
+end
+ 
+function dig()
+		turtle.dig()
+		sleep(.5)
+		turtle.forward()
+		sleep(.6)
+end
+ 
+ 
+function autoFuel()
+        io.write "Turn auto-fuel on? (y/n): "
+        f = io.read()
+        if f ~= "y" then
+                if f ~= "n" then
+                        print ""
+                        print "Please enter a valid response"
+                        print ""
+                        autoFuel()
+                else
+                start()
+                end
+        else
+        start()
+        end
+end
+ 
+function start()
+        io.write "Length of Landscape: "
+        d = io.read()
+        if not tonumber(d) then
+                print ""
+                print "Please enter a valid number"
+                print ""
+                start()
+        else
+                d = tonumber(d)
+        end
+		
+		io.write "Width of Landscape: "
+		e not tonumber(e) then
+				print ""
+				print "Please enter a valid number"
+				print ""
+				start()
+		else
+			e = tonumber(e)
+		end
+				
+		
+        if d < 1 then
+                print ""
+                print "Number is not valid"
+                print ""
+                start()
+        end
+		
+		if e < 1 then
+				print ""
+                print "Number is not valid"
+                print ""
+                start()
+		end
+end
+ 
+function addFuel()
+        print ""
+        print "Add fuel to continue..."
+        turtle.select(1)
+        while turtle.getFuelLevel() == 0 do
+                turtle.refuel()
+                sleep(1)
+        end
+        print ""
+        print "Continuing to Mine"
+        print ""
+end
+ 
+ 
+function fuelCheck()
+        if turtle.getFuelLevel() == 0 then
+                if f == "y" then
+                        for i = 1, 16 do
+                                turtle.select(i)
+                                turtle.refuel(1)
+                                if turtle.getFuelLevel() > 0 then
+                                        turtle.select(1)
+                                        break
+                                end
+                        end
+                if turtle.getFuelLevel() == 0 then
+                        addFuel()
+                end
+                else
+                        addFuel()
+                end
+        end
+end
+
+t = 0
+text()
+autoFuel()
+turtle.refuel()
+print ""
+print "Beginning Mining Process"
+turtle.digDown()
+turtle.down()
+for z = 1, e do
+	for i = 1, d do
+			fuelCheck()
+			dig()
+	end
+	t = t + 1
+	if t%2 == 0
+		turtle.turnLeft();
+		dig();
+		turtle.turnLeft();
+	else
+		turtle.turnRight();
+		dig();
+		turtle.turnRight();
+	end
+end
+
+print ""
+print "Mining Complete!"
+print "Timothy Roe Jr."
